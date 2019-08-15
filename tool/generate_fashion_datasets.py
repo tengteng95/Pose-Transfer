bfaa7c3a57fb611c836120f00041f81626850d09
tthuang <982639009@qq.com>
2019-08-15 15:16:26 +0800

    update

--- tool/generate_fashion_datasets.py
+++ tool/generate_fashion_datasets.py
@@ -1,38 +1,62 @@
 import os
+import shutil
+from PIL import Image
 
-# path for downloaded fashion images
-root_fashion_dir = ''
-assert len(root_fashion_dir) > 0, 'please give the path of raw deep fashion dataset!'
+IMG_EXTENSIONS = [
+'.jpg', '.JPG', '.jpeg', '.JPEG',
+'.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+]
 
-train_images = []
-train_f = open('fashion_data/train.lst', 'r')
-for lines in train_f:
-	lines = lines.strip()
-	if lines.endswith('.jpg'):
-		train_images.append(lines)
+def is_image_file(filename):
+	return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
-test_images = []
-test_f = open('fashion_data/test.lst', 'r')
-for lines in test_f:
-	lines = lines.strip()
-	if lines.endswith('.jpg'):
-		test_images.append(lines)
+def make_dataset(dir):
+	images = []
+	assert os.path.isdir(dir), '%s is not a valid directory' % dir
+	new_root = './fashion_data'
+	if not os.path.exists(new_root):
+		os.mkdir(new_root)
 
-train_path = 'fashion_data/train'
-if not os.path.exists(train_path):
-	os.mkdir(train_path)
+	train_root = './fashion_data/train'
+	if not os.path.exists(train_root):
+		os.mkdir(train_root)
 
-for item in train_images:
-	from_ = os.path.join(root_fashion_dir, item)
-	to_ = os.path.join(train_path, item)
-	os.system('cp %s %s' %(from_, to_))
+	test_root = './fashion_data/test'
+	if not os.path.exists(test_root):
+		os.mkdir(test_root)
 
+	train_images = []
+	train_f = open('./fashion_data/train.lst', 'r')
+	for lines in train_f:
+		lines = lines.strip()
+		if lines.endswith('.jpg'):
+			train_images.append(lines)
 
-test_path = 'fashion_data/test'
-if not os.path.exists(test_path):
-	os.mkdir(test_path)
+	test_images = []
+	test_f = open('./fashion_data/test.lst', 'r')
+	for lines in test_f:
+		lines = lines.strip()
+		if lines.endswith('.jpg'):
+			test_images.append(lines)
 
-for item in test_images:
-	from_ = os.path.join(root_fashion_dir, item)
-	to_ = os.path.join(test_path, item)
-	os.system('cp %s %s' %(from_, to_))
+	print(train_images, test_images)
+	
+
+	for root, _, fnames in sorted(os.walk(dir)):
+		for fname in fnames:
+			if is_image_file(fname):
+				path = os.path.join(root, fname)
+				path_names = path.split('/') 
+				# path_names[2] = path_names[2].replace('_', '')
+				path_names[3] = path_names[3].replace('_', '')
+				path_names[4] = path_names[4].split('_')[0] + "_" + "".join(path_names[4].split('_')[1:])
+				path_names = "".join(path_names)
+				# new_path = os.path.join(root, path_names)
+				img = Image.open(path)
+				imgcrop = img.crop((40, 0, 216, 256))
+				if new_path in train_images:
+					imgcrop.save(os.path.join(train_root, path_names))
+				elif new_path in test_images:
+					imgcrop.save(os.path.join(test_root, path_names))
+
+make_dataset('./fashion')
